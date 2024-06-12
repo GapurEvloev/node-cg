cdf31e7bd25f0da94697805408353d3eafc36385
gapur <kaloycant@gmail.com>
2024-06-12 10:55:37 +0200

    34 done

---
+++ src/app.ts
@@ -0,0 +1,52 @@
+import http from 'node:http';
+import fs from 'node:fs';
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    const { url, method } = req;
+    if (url === '/favicon.ico') {
+        res.writeHead(404);
+        res.end();
+        return;
+    }
+    if (url === '/') {
+        res.write(`
+            <html>
+                <head><title>Enter message</title></head>
+                <body>
+                    <form action="/message" method="POST">
+                        <input type="text" name="message">
+                        <button type="submit">Send</button>
+                    </form>
+                </body>
+            </html>
+        `);
+        return res.end();
+    };
+
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        // res.writeHead(302, {
+        //     'Location': '/'
+        // });
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    };
+
+    res.write(`
+        <html>
+            <head><title>My first page</title></head>
+            <body>
+                <h1>Hello from my Node.js server!</h1>
+            </body>
+        </html>
+    `);
+    res.end();
+});
+
+server.listen(port, () => {
+    console.log('Server started on http://localhost:3000');
+});
