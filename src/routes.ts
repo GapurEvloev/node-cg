772f014a7606ba7bd06b9d12baa032039a1c039e
gapur <kaloycant@gmail.com>
2024-06-15 21:02:39 +0200

    S3 done

--- src/routes.ts
+++ src/routes.ts
@@ -8,20 +8,52 @@ const requestHandler = (req: any, res: any) => {
         res.end();
         return;
     }
-    if (url === '/') {
+    if (url === "/") {
         res.write(`
-            <html>
-                <head><title>Enter message</title></head>
-                <body>
-                    <form action="/message" method="POST">
-                        <input type="text" name="message">
-                        <button type="submit">Send</button>
-                    </form>
-                </body>
-            </html>
-        `);
+                <html>
+                    <head><title>Hello</title></head>
+                    <body>
+                        <form action="/create-user" method="POST">
+                            <input type="text" name="username">
+                            <button type="submit">Create</button>
+                        </form>
+                    </body>
+                </html>
+            `);
+        console.log("Hello");
         return res.end();
-    };
+    }
+
+    if (url === "/users") {
+        res.write(`
+                <html>
+                    <head><title>Users</title></head>
+                    <body>
+                        <ul>
+                            <li>user 1</li>
+                            <li>user 2</li>
+                        </ul>
+                    </body>
+                </html>
+            `);
+        console.log("Users");
+        return res.end();
+    }
+
+    if (url === "/create-user" && method === "POST") {
+        const body: any[] = [];
+        req.on("data", (chunk: any) => {
+            body.push(chunk);
+        });
+        return req.on("end", () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const user = parsedBody.split("=")[1];
+            console.log({ user });
+            res.statusCode = 302;
+            res.setHeader("Location", "/users");
+            return res.end();
+        });
+    }
 
     if (url === '/message' && method === 'POST') {
         const body: any[] = [];
