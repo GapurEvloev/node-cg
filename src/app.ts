415918df4ddec35654461300f918ea358ab54ca0
gapur <kaloycant@gmail.com>
2024-06-16 15:17:19 +0200

    s5 l64 done

--- src/app.ts
+++ src/app.ts
@@ -6,20 +6,26 @@ const port = 3000;
 // app.use(express.urlencoded({ extended: true })); // option 1
 app.use(express.json()); // option 2
 
-// // // option 1 - from course
-// app.use((req: Request, res: Response, next: NextFunction) => {
-//     console.log('Request received');
-//     next();
-// });
-// app.use((req: Request, res: Response, next: NextFunction) => {
-//     console.log('Request received 2');
-//     res.send('<h1>Hello from Express!</h1>');
-// });
-
-// // option 2 - my version
-app.get('/', (req: Request, res: Response) => {
-    res.send('<h1>Hello, Express with TypeScript!</h1>');
+app.use((req: Request, res: Response, next: NextFunction) => {
+    console.log('This always runs!');
+    next();
 });
+
+// // option 1 - from course
+app.use('/product', (req: Request, res: Response, next: NextFunction) => {
+    console.log('Request received 2');
+    res.send('<h1>Welcome to Product Page!</h1>');
+});
+
+app.use('/', (req: Request, res: Response, next: NextFunction) => {
+    console.log('Request received 2');
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+// // // option 2 - my version
+// app.get('/', (req: Request, res: Response) => {
+//     res.send('<h1>Hello, Express with TypeScript!</h1>');
+// });
 
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
