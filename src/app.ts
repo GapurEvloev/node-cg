372c29c6d23241ee574e39acb2fb7374b6e0b4bb
gapur <kaloycant@gmail.com>
2024-06-16 18:16:30 +0200

    s5 l68 - error handling

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(admin);
 app.use(shop);
 
+// error handling middleware
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
+
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
 });
