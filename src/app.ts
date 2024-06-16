a8056798a937b51bf58445d34bbcdfe7f051f3bb
gapur <kaloycant@gmail.com>
2024-06-16 17:54:46 +0200

    s5 l65, l66 done

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,29 @@
 import express, { Request, Response, NextFunction } from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
 
-// app.use(express.urlencoded({ extended: true })); // option 1
-app.use(express.json()); // option 2
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req: Request, res: Response, next: NextFunction) => {
-    console.log('This always runs!');
-    next();
+app.use('/add-product', (req: Request, res: Response, next: NextFunction) => {
+    res.send(`
+        <form action="/product" method="POST">
+            <input type="text" name="title">
+            <button type="submit">Add Product</button>
+        </form>
+    `);
 });
 
-// // option 1 - from course
-app.use('/product', (req: Request, res: Response, next: NextFunction) => {
-    console.log('Request received 2');
-    res.send('<h1>Welcome to Product Page!</h1>');
+app.post('/product', (req: Request, res: Response, next: NextFunction) => {
+    console.log('Product added');
+    console.log(req.body);
+    res.redirect('/');
 });
 
-app.use('/', (req: Request, res: Response, next: NextFunction) => {
-    console.log('Request received 2');
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.send('<h1>Hello from Express!</h1>');
 });
-
-// // // option 2 - my version
-// app.get('/', (req: Request, res: Response) => {
-//     res.send('<h1>Hello, Express with TypeScript!</h1>');
-// });
 
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
