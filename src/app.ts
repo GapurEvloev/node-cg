045d1c4e6958c35f115f0e8f77ff2659cfcedb24
gapur <kaloycant@gmail.com>
2024-06-16 13:55:26 +0200

    s5 l63 done

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,26 @@
-import http from 'node:http';
-import requestHandler from './routes';
+import express, { Request, Response, NextFunction } from 'express';
 
+const app = express();
 const port = 3000;
 
-const server = http.createServer(requestHandler);
+// app.use(express.urlencoded({ extended: true })); // option 1
+app.use(express.json()); // option 2
 
-server.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}`);
+// // // option 1 - from course
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//     console.log('Request received');
+//     next();
+// });
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//     console.log('Request received 2');
+//     res.send('<h1>Hello from Express!</h1>');
+// });
+
+// // option 2 - my version
+app.get('/', (req: Request, res: Response) => {
+    res.send('<h1>Hello, Express with TypeScript!</h1>');
+});
+
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
 });
